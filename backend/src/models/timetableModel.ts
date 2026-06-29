304bec9045d317b71154a89f291235401d843831
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-06-29 17:37:27 +0300

    feat: add Timetable model with timeslots sub-document

---
+++ backend/src/models/timetableModel.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document, ObjectId, Schema } from "mongoose";
+
+interface TimeSlot {
+  teacherId: ObjectId;
+  subjectId: ObjectId;
+  day: "SAT" | "SUN" | "MON" | "TUE" | "WED" | "THU";
+  startTime: Date;
+  endTime: Date;
+  location: string;
+}
+
+export interface ITimetable extends Document {
+  gradeId: ObjectId;
+  timeSlots: TimeSlot[];
+  generatedBy: ObjectId;
+}
+
+const timetableSchema = new Schema<ITimetable>({
+  gradeId: { type: mongoose.Schema.ObjectId, ref: "Grade", required: true },
+  timeSlots: [
+    {
+      teacherId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Teacher",
+        required: true,
+      },
+      subjectId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Subject",
+        required: true,
+      },
+      location: { type: String, required: true },
+      day: {
+        type: String,
+        enum: ["SAT", "SUN", "MON", "TUE", "WED", "THU"],
+        required: true,
+      },
+      startTime: { type: Date, required: true },
+      endTime: { type: Date, required: true },
+    },
+  ],
+  generatedBy: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+});
+
+timetableSchema.index({ gradeId: 1 }, { unique: true });
+
+const Timetable = mongoose.model<ITimetable>("Timetable", timetableSchema);
+
+export default Timetable;
